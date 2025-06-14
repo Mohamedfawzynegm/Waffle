7867c500562535c8a2f42e5e3ce5eb0f3d62e196
Mohamed Fawzy Negm <157843257+Mohamedfawzynegm@users.noreply.github.com>
2025-06-14 23:24:03 +0300

    Update waffle.js

--- waffle.js
+++ waffle.js
@@ -5,7 +5,7 @@ Form.addEventListener("submit", (e) => {
     let messege = [];
     if (quantity.value > 10 || quantity.value == "" || quantity.value < 1) {
         e.preventDefault();
-        messege.push('Please enter a proper quantity');
+        messege.push('Please enter a proper quantity, and less than 10"');
         error.innerText = messege.join(', ');
     }
     else {
